c6c490f4cc1948e7c4dd9fdad39197f0e797c381
Accelerator086 <32789300+Accelerator086@users.noreply.github.com>
2020-04-04 18:01:23 +0800

    Add files via upload

---
+++ scrapy practice 31 csgowallpaper.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+import urllib
+import time
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'
+}
+
+final_folder = './csgowallpaper/'
+os.makedirs(final_folder, exist_ok=True)
+
+proxies_v2ray = {
+'http':'http://127.0.0.1:10809',
+'https':'https://127.0.0.1:10809'
+}
+
+opener = urllib.request.build_opener(urllib.request.ProxyHandler(proxies_v2ray))
+urllib.request.install_opener(opener)
+
+for n in range(1,2746):
+    url_original='https://csgowallpapers.com/include/download_wallpaper.php?id='+str(n)
+    if os.access(final_folder+'%s.png'%str(n), os.F_OK):
+        print('图片{}存在，跳过'.format(n))
+    else:
+        urllib.request.urlretrieve(url_original,final_folder+'%s.png'%str(n))
+        print('下载{}图片完成'.format(n))
+        time.sleep(0.5)
+print('Finish!')
